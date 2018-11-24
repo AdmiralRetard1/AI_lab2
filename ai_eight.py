f142747522ee44e29582ac6a862fec8102236280
Alex Ivanov <alexivanov04121994@gmail.com>
2018-11-24 14:24:38 +0300

    Added second way to find solution

--- ai_eight.py
+++ ai_eight.py
@@ -5,4 +5,10 @@ start_layout = node.input_layout("Enter starting layout: ")
 target_layout = node.input_layout("Enter target layout: ")
 start_node = node.Node(start_layout, target_layout)
 target_node = node.Node(target_layout, target_layout, level=10000)
-algorithms.find_solution_informed(start_node, target_node)
+print("\nChoose algorithm:")
+print("1 - Manhattan ways")
+print("2 - Tiles not on their position")
+if input() == "1":
+    algorithms.find_solution_informed_manhattan(start_node, target_node)
+else:
+    algorithms.find_solution_informed_tiles(start_node, target_node)
--- node.py
+++ node.py
@@ -24,6 +24,17 @@ class Node(object):
         return True if self.layout == comp_node.layout else False
 
 
+# class for 2nd euristic
+class Node2(Node):
+    # function to enter layout with checking it for uniqueness and fullness
+    def __init__(self, new_layout, target_layout, old_node=None, level=0, ):
+        self.layout = new_layout
+        self.level = level
+        # cost to move 1 tile compared to previous layout
+        self.wayCost = count_cost2(target_layout, new_layout, old_node)
+        self.prev_node_layout = old_node.layout if old_node else None
+
+
 # list class expansion to represent nodes
 class NodeList(list):
 
@@ -33,13 +44,23 @@ class NodeList(list):
         return key.layout in layouts
 
 
-# function that return the value of tile in new layout that was moved on empty position
+# function that counts the cost of way of current node
 def count_cost(target_layout, new_layout, old_node=None):
     sum_of_moves = 0
     for n in new_layout:
         sum_of_moves += abs(new_layout.index(n) % 3 - target_layout.index(n) % 3) + \
                         abs(new_layout.index(n) // 3 - target_layout.index(n) // 3)
-    way_cost = int(new_layout[old_node.layout.index(" ")]) + old_node.wayCost + sum_of_moves if old_node else 0
+    way_cost = old_node.level + old_node.wayCost + sum_of_moves if old_node else 0
+    return way_cost
+
+
+# function that counts the cost of way of current node
+def count_cost2(target_layout, new_layout, old_node=None):
+    counter = 0
+    for n in new_layout:
+        if new_layout.index(n) != target_layout.index(n):
+            counter += 1
+    way_cost = old_node.wayCost + counter if old_node else 0
     return way_cost
 
 
@@ -69,8 +90,8 @@ def input_layout(message):
     return layout
 
 
-# universal func to print our lists without braces
-def printlist(lst):
+# universal func to print our lists without braces and with wayCost under layout
+def print_nodes(lst):
     lines = ["", "", "", ""]
     res = ""
     if lst:
@@ -85,6 +106,27 @@ def printlist(lst):
             if nodeCounter == 30:
                 res += "\n".join(lines) + "\n\n"
                 lines = ["", "", "", ""]
+                nodeCounter = 0
+        res += "\n".join(lines)
+        print(res)
+    else:
+        print("Empty list")
+
+
+# this does the same as previou function, but without wayCost
+def print_list(lst):
+    lines = ["", "", ""]
+    res = ""
+    if lst:
+        nodeCounter = 0
+        for n in lst:
+            lines[0] += n[:3] + "  "
+            lines[1] += n[3:6] + "  "
+            lines[2] += n[6:] + "  "
+            nodeCounter += 1
+            if nodeCounter == 30:
+                res += "\n".join(lines) + "\n\n"
+                lines = ["", "", ""]
                 nodeCounter = 0
         res += "\n".join(lines)
         print(res)
